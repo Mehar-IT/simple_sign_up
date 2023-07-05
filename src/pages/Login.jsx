edabfe1181a61f0528a8bea19d8d83ae04edb81c
Mehar-It <hamzatariquearain@gmail.com>
2023-07-05 17:57:54 +0500

    added login validation

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+import { useFormik } from "formik";
+import { loginSchema } from "../utils/schemas";
+import login from "../assets/login.jpg";
+
+const initialValues = {
+  email: "",
+  password: "",
+};
 
 export default function Login() {
+  const [open, setOpen] = useState(false);
+  const { values, errors, touched, handleBlur, handleChange, handleSubmit } =
+    useFormik({
+      initialValues,
+      validationSchema: loginSchema,
+      onSubmit: (values, action) => {
+        alert(
+          `🚀 ~ file: Registration.jsx ~ line 11 ~ Registration ~ values:${JSON.stringify(
+            values
+          )}`
+        );
+        action.resetForm();
+      },
+    });
   return (
     <section className="bg-gray-50 min-h-screen flex items-center justify-center">
       <div className="bg-gray-200 flex rounded-2xl shadow-lg max-w-3xl p-5 items-center">
@@ -10,33 +32,87 @@ export default function Login() {
             If you are already a member, easily log in
           </p>
 
-          <form action="" className="flex flex-col gap-4">
-            <input
-              className="p-2 mt-8 rounded-xl border"
-              type="email"
-              name="email"
-              placeholder="Email"
-            />
-            <div className="relative">
+          <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+            <div>
               <input
-                className="p-2 rounded-xl border w-full"
-                type="password"
-                name="password"
-                placeholder="Password"
+                className="p-2 mt-8 rounded-xl border w-full"
+                type="email"
+                autoComplete="off"
+                name="email"
+                id="email"
+                placeholder="Enter email here"
+                value={values.email}
+                onChange={handleChange}
+                onBlur={handleBlur}
               />
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="16"
-                height="16"
-                fill="gray"
-                className="bi bi-eye absolute top-1/2 right-3 -translate-y-1/2"
-                viewBox="0 0 16 16"
-              >
-                <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z" />
-                <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z" />
-              </svg>
+              {errors.email && touched.email ? (
+                <p className="text-red-600 text-xs pl-2">{errors.email}</p>
+              ) : null}
             </div>
-            <button className="bg-[#002D74] rounded-xl text-white py-2 hover:scale-105 duration-300">
+
+            <div>
+              <div className="relative">
+                <input
+                  className="p-2 rounded-xl border w-full"
+                  type="password"
+                  autoComplete="off"
+                  name="password"
+                  id="password"
+                  placeholder="Enter password here"
+                  value={values.password}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                />
+
+                <span
+                  className="hover:cursor-pointer"
+                  onClick={() => {
+                    let pass = document.getElementById("password");
+                    console.log(pass.type);
+                    if (pass.type === "password") {
+                      pass.type = "text";
+                    } else {
+                      pass.type = "password";
+                    }
+                    setOpen(!open);
+                  }}
+                >
+                  {open ? (
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="16"
+                      height="16"
+                      fill="gray"
+                      className="bi bi-eye absolute top-1/2 right-3 -translate-y-1/2"
+                      viewBox="0 0 16 16"
+                    >
+                      <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z" />
+                      <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z" />
+                    </svg>
+                  ) : (
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="16"
+                      height="16"
+                      fill="gray"
+                      className="bi bi-eye-slash absolute top-1/2 right-3 -translate-y-1/2"
+                      viewBox="0 0 16 16"
+                    >
+                      <path d="M13.359 11.238C15.06 9.72 16 8 16 8s-3-5.5-8-5.5a7.028 7.028 0 0 0-2.79.588l.77.771A5.944 5.944 0 0 1 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.134 13.134 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755-.165.165-.337.328-.517.486l.708.709z" />
+                      <path d="M11.297 9.176a3.5 3.5 0 0 0-4.474-4.474l.823.823a2.5 2.5 0 0 1 2.829 2.829l.822.822zm-2.943 1.299.822.822a3.5 3.5 0 0 1-4.474-4.474l.823.823a2.5 2.5 0 0 0 2.829 2.829z" />
+                      <path d="M3.35 5.47c-.18.16-.353.322-.518.487A13.134 13.134 0 0 0 1.172 8l.195.288c.335.48.83 1.12 1.465 1.755C4.121 11.332 5.881 12.5 8 12.5c.716 0 1.39-.133 2.02-.36l.77.772A7.029 7.029 0 0 1 8 13.5C3 13.5 0 8 0 8s.939-1.721 2.641-3.238l.708.709zm10.296 8.884-12-12 .708-.708 12 12-.708.708z" />
+                    </svg>
+                  )}
+                </span>
+              </div>
+              {errors.password && touched.password ? (
+                <p className="text-red-600 text-xs pl-2">{errors.password}</p>
+              ) : null}
+            </div>
+            <button
+              type="submit"
+              className="bg-[#002D74] rounded-xl text-white py-2 hover:scale-105 duration-300"
+            >
               Login
             </button>
           </form>
@@ -87,10 +163,7 @@ export default function Login() {
         </div>
 
         <div className="md:block hidden w-1/2">
-          <img
-            className="rounded-2xl"
-            src="https://images.unsplash.com/photo-1616606103915-dea7be788566?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1887&q=80"
-          />
+          <img className="rounded-2xl h-[480px]" src={login} />
         </div>
       </div>
     </section>
